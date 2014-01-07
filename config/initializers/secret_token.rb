9c7e30056ddd97e314b8e50b46fa767ad2e30f52
jharveygroupize <jharvey@groupize.com>
2014-01-07 13:34:48 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omrails::Application.config.secret_key_base = '9893e195a9c3cd826dc508dcadb614d869747de7833ad57c94486156e41ffef19efa231a086e0059d56f5581e0b6ee1a607c2f8b541580ad4d084cd47d70f76d'
